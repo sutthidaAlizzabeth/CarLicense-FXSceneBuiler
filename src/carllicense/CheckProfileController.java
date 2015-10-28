cae8b420d24b593b70cf2a4b6e05cd1e139a6678
Alizzabeth <Alizzabeth@sutthidas-MacBook-Pro.local>
2015-10-28 23:34:15 +0700

    @ตรวจประวัติ => เพิ่มข้อมูลและจัดกลุ่มข้อมูล

--- src/carllicense/CheckProfileController.java
+++ src/carllicense/CheckProfileController.java
@@ -7,7 +7,13 @@ package carllicense;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.shape.Rectangle;
 
 /**
  * FXML Controller class
@@ -15,6 +21,32 @@ import javafx.fxml.Initializable;
  * @author mazzrider
  */
 public class CheckProfileController implements Initializable {
+    @FXML
+    private TextField search;
+    @FXML
+    private Button btn_search;
+    @FXML
+    private Rectangle data_bg1;
+    @FXML
+    private Rectangle data_bg2;
+    @FXML
+    private Button data_btn;
+    @FXML
+    private GridPane data1;
+    @FXML
+    private GridPane data2;
+    @FXML
+    private GridPane data3;
+    @FXML
+    private Rectangle data_bg3;
+    @FXML
+    private Button btn_license;
+    @FXML
+    private Button btn_history;
+    @FXML
+    private Button btn_home;
+    @FXML
+    private Button btn_next;
 
     /**
      * Initializes the controller class.
@@ -23,5 +55,17 @@ public class CheckProfileController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void searchId(ActionEvent event) {
+    }
+
+    @FXML
+    private void goToLicense(ActionEvent event) {
+    }
+
+    @FXML
+    private void goToHistory(ActionEvent event) {
+    }
     
 }
