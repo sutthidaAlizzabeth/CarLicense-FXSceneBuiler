59d7e02bd3f50604b11801be2f584f444387b31e
Alizzabeth <Alizzabeth@sutthidas-MacBook-Pro.local>
2015-10-29 01:10:15 +0700

    @หน้าตรวจประวัติ => แก้ไขการแสดงผลเริ่มแรก ให้ไม่แสดงข้อมูลอะไรเลย

--- src/carllicense/CheckProfileController.java
+++ src/carllicense/CheckProfileController.java
@@ -14,6 +14,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
 
 /**
  * FXML Controller class
@@ -47,6 +48,8 @@ public class CheckProfileController implements Initializable {
     private Button btn_home;
     @FXML
     private Button btn_next;
+    @FXML
+    private Text result;
 
     /**
      * Initializes the controller class.
