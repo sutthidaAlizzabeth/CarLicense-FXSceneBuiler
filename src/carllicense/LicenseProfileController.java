5318f06fa0be669498640b3d7118afd5dae0f1bf
Alizzabeth <Alizzabeth@sutthidas-MacBook-Pro.local>
2015-10-29 11:10:54 +0700

    @หน้า "ประวัติใบขับขี่" =>  เพิ่ม function ลิงค์ไปหน้า "หน้าแรก" และ ลิงค์ไปหน้า "ตรวจประวัติ"

--- src/carllicense/LicenseProfileController.java
+++ src/carllicense/LicenseProfileController.java
@@ -5,9 +5,17 @@
  */
 package carllicense;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -15,6 +23,10 @@ import javafx.fxml.Initializable;
  * @author Predator
  */
 public class LicenseProfileController implements Initializable {
+    @FXML
+    private Button btn_profile;
+    @FXML
+    private Button btn_home;
 
     /**
      * Initializes the controller class.
@@ -23,5 +35,23 @@ public class LicenseProfileController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void goToProfile(ActionEvent event) throws IOException {
+        Parent page = FXMLLoader.load(getClass().getResource("checkProfile.fxml"));
+        Scene scene = new Scene(page);
+        Stage stage = (Stage) btn_profile.getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    private void goToHome(ActionEvent event) throws IOException {
+        Parent page = FXMLLoader.load(getClass().getResource("menuSelect.fxml"));
+        Scene scene = new Scene(page);
+        Stage stage = (Stage) btn_profile.getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
     
 }
