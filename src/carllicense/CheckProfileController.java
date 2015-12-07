4087dc999390397118c36526eb72ce27a0be7132
Alizzabeth <Alizzabeth@sutthidas-MacBook-Pro.local>
2015-12-08 00:40:04 +0700

    no message

---
+++ src/carllicense/CheckProfileController.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package carllicense;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author mazzrider
+ */
+public class CheckProfileController implements Initializable {
+    @FXML
+    private TextField search;
+    @FXML
+    private Button btn_search;
+    @FXML
+    private Button data_btn;
+    @FXML
+    private GridPane data1;
+    @FXML
+    private GridPane data2;
+    @FXML
+    private Button btn_home;
+    @FXML
+    private Button btn_next;
+    @FXML
+    private Text result;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void searchId(ActionEvent event) {
+        String id = search.getText();
+        String b_id = "1101500764377";
+        data_btn.setVisible(id.equals(b_id));
+        data1.setVisible(id.equals(b_id));
+        data2.setVisible(id.equals(b_id));
+        if(!id.equals(b_id)){
+            result.setText("ไม่พบข้อมูล");
+        }
+        result.setVisible(!id.equals(b_id) || id == null);
+    }
+
+
+    @FXML
+    private void goToHistory(ActionEvent event) throws IOException {
+        Parent page = FXMLLoader.load(getClass().getResource("licenseProfile.fxml"));
+        Scene scene = new Scene(page);
+        Stage stage = (Stage) btn_next.getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    private void goToHome(ActionEvent event) throws IOException {
+        Parent page = FXMLLoader.load(getClass().getResource("menuSelect.fxml"));
+        Scene scene = new Scene(page);
+        Stage stage = (Stage) btn_home.getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }    
+}
